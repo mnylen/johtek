c1cbc79bdc89ccf8c47a38e8337ca8c7d39b3ab0
mnylen <mikko.nylen@gmail.com>
2011-09-18 09:27:35 +0300

    Reittiopas...

---
+++ laskarit2/tehtava3/reittiopas.py
@@ -0,0 +1,216 @@
+MAXIMUM_SPEED = 10
+
+class Node(object):
+    def __init__(self, parent, stop, time, line = None):
+        self.parent = parent
+        self.stop   = stop
+        self.time   = time
+        self.line   = line
+        
+    def adjacent_nodes(self):
+        nodes = set()
+        
+        for line in self.stop.transportations:
+            waiting_time = line.calculate_waiting_time(self.time, self.stop)
+            next_stop    = line.next_stop(self.stop)
+            
+            if next_stop != None:
+                travel_time       = line.calculate_travel_time(self.stop, next_stop)
+                time_at_next_stop = self.time + waiting_time + travel_time
+                
+                nodes.add(Node(self, next_stop, time_at_next_stop, line))
+        
+        return nodes
+        
+    def h_cost(self, destination_stop):
+        distance = self.stop.distance(destination_stop)
+        return int(distance / MAXIMUM_SPEED)
+        
+    def g_cost(self):
+        self.time
+        
+    def f_cost(self, destination_stop):
+        return self.g_cost() + self.h_cost()
+        
+    def __eq__(self, other):
+        return self.stop == other.stop and self.time == other.time
+    
+    def __hash__(self, other):
+        return hash(self.stop) * 37 + hash(self.time) * 37
+
+class Stop(object):
+    def __init__(self, number, y_coord, x_coord, name):
+        self.number          = number
+        self.y_coord         = y_coord
+        self.x_coord         = x_coord
+        self.name            = name
+        self.transportations = set()
+    
+    def add_transportation(self, line):
+        self.transportations.add(line)
+    
+    def distance(self, other):
+        x_distance = abs(other.x_coord - self.x_coord)
+        y_distance = abs(other.y_coord - self.y_coord)
+        
+        from math import sqrt
+        total_distance = sqrt(x_distance*x_distance, y_distance*y_distance)
+        
+        return total_distance
+        
+    def __eq__(self, other):
+        return self.number == other.number
+    
+    def __hash__(self):
+        return hash(self.number)
+
+class Transportation(object):
+    def __init__(self, name):
+        self.name       = name
+        self.time_table = []
+    
+    def __eq__(self, other):
+        return self.name == other.name
+        
+    def __hash__(self):
+        return hash(self.name)
+            
+    def get_time_spent_on_arrival_at(self, stop):
+        return self.time_table[stop.number-1][1]
+        
+    def first_stop(self, stop):
+        return self.time_table[0][0]
+    
+    def add_time_table_entry(self, stop, time_spent_from_departure):
+        self.time_table.append( (stop, time_spent_from_departure) )
+        stop.add_transportation(self)
+    
+    def calculate_travel_time(self, from_stop, to_stop):
+        """
+        Calculates the time in minutes it takes for this transportation
+        to travel from the given stop to the given stop.
+        
+        """
+        
+        return self.get_time_spent_on_arrival_at(to_stop) - self.get_time_spent_on_arrival_at(from_stop)
+        
+    
+    def calculate_waiting_time(self, current_time, stop):
+        """
+        Calculates the minutes we need to wait at the stop for
+        this transportation to arrive there.
+        
+        """
+        
+        travel_time  = self.calculate_travel_time(self.first_stop, stop)
+        waiting_time = 0
+        
+        # a) Look backward from current_time to see if there's
+        #    a departure that arrives at the stop after current_time.
+        
+        remainder      = (current_time % 10)
+        departure_time = current_time - remainder
+        
+        while departure_time + travel_time > current_time:
+            departure_time -= 10
+        
+        if departure_time + travel_time <= current_time:
+            # we just missed that departure
+            departure_time += 10
+        
+        waiting_time = (departure_time + travel_time) - current_time
+        
+        
+        # b) Look forward from current_time to find a departure
+        #    that arrives at the stop after current_time
+        
+        departure_time = current_time - remainder + 10
+        while departure_time + travel_time < current_time:
+            departure_time += 10
+                
+        # Pick the departure that results in the lowest waiting time
+        waiting_time = min(waiting_time, (departure_time + travel_time) - current_time)
+        
+        return waiting_time
+        
+    def next_stop(self, current_stop):
+        current_stop_index = current_stop.number - 1
+        
+        if current_stop_index < 0 or current_stop_index > len(self.time_table)-1:
+            return None
+        
+        next_entry = self.time_table[current_stop_index+1]
+        return next_entry[0]
+
+
+def astar(self, from_stop, to_stop):
+    from Queue import PriorityQueue
+    
+    start_node  = Node(None, from_stop, 0)
+    closed_list = set()
+    
+    open_list   = PriorityQueue()
+    open_list.put( (start_node.f_cost(to_stop), start_node) )
+    
+    found = False
+    target_node = None
+    
+    while not open_list.empty():
+        f_cost, current = open_list.get()
+        closed_list.add(current)
+        
+        if current.stop == to_stop:
+            found = True
+            target_node = current
+            break
+        
+        
+        for adjacent_node in current.adjacent_nodes():
+            if adjacent_node in closed_list:
+                continue
+                
+            adjacent_node.parent = current
+            open_list.add(current)
+        
+    if found:
+        path = []
+        current = target_node
+        
+        while current is not None:
+            path.append(current)
+            current = current.parent
+            
+        path.reverse()
+        return path
+    else:
+        return None
+
+def read_data():
+    f     = open("data/pysakit.txt", "r")
+    stops = dict()
+    
+    for stop_data in f.readlines():
+        stop_data = stop_data.strip()
+        
+        if stop_data == "":
+            continue
+        
+        number, y_coord, x_coord, name = stop_data.split(" ")
+        stops[name] = Stop(number, y_coord, x_coord, name)
+    
+    f.close()
+    
+    
+
+if __name__ == "__main__":
+    import sys
+    
+    if len(sys.argv) < 3:
+        print "Usage: python %s FROM_STOP TO_STOP" % sys.argv[0]
+        exit(1)
+    
+    from_stop_name        = sys.argv[1]
+    destination_stop_name = sys.argv[2]
+    
+    # Load data from files
+    path = astar(from_stop, to_stop)
