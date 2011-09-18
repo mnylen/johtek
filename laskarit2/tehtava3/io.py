ed8a05e7ac15cd452c6ab71b4b6afbeadbc3955b
mnylen <mikko.nylen@gmail.com>
2011-09-18 17:23:07 +0300

    Refactored reittiopas

---
+++ laskarit2/tehtava3/io.py
@@ -0,0 +1,47 @@
+from reittiopas import Transportation, Stop
+
+def read_data():
+    stops_by_name   = dict()
+    stops_by_number = dict()
+    
+    f = open("data/pysakit.txt", "r")
+    for stop_data in f.readlines():
+        stop_data = stop_data.strip()
+        
+        if stop_data == "":
+            continue
+        
+        number, y_coord, x_coord, name = stop_data.split(" ")
+        number  = int(number)
+        y_coord = int(y_coord)
+        x_coord = int(x_coord)
+        stop    = Stop(number, y_coord, x_coord, name)
+        
+        stops_by_name[name]     = stop
+        stops_by_number[number] = stop
+        
+    f.close()
+    
+    lines = set()
+    
+    for line_name in ["11", "11b", "1a", "1ab", "3b", "3t", "55k", "55kb", "7a", "7b", "M", "Mb"]:
+        line = Transportation(line_name)
+        
+        f = open("data/linja%s.txt" % line_name, "r")
+        for time_table_entry in f.readlines():
+            time_table_entry = time_table_entry.strip()
+            
+            if time_table_entry == "":
+                continue
+            
+            stop_number, time_spent_str = time_table_entry.split(" ")
+            stop_number = int(stop_number)
+            time_spent  = int(time_spent_str)
+            stop        = stops_by_number[stop_number]
+            
+            line.add_time_table_entry(stop, time_spent)
+        
+        f.close()
+        lines.add(line)
+                
+    return (stops_by_name, stops_by_number, lines)
--- laskarit2/tehtava3/reittiopas.py
+++ laskarit2/tehtava3/reittiopas.py
@@ -11,31 +11,21 @@ class Node(object):
         nodes = set()
         
         for line in self.stop.transportations:
-            if self.line and line == self.line:
-                waiting_time = 0
-            else:
-                waiting_time = line.calculate_waiting_time(self.time, self.stop)
-            
-            next_stop    = line.next_stop(self.stop)
+            next_stop = line.next_stop(self.stop)
             
             if next_stop != None:
+                if self.line and line == self.line:
+                    waiting_time = 0
+                else:
+                    waiting_time = line.calculate_waiting_time(self.time, self.stop)
+                
                 travel_time       = line.calculate_travel_time(self.stop, next_stop)
                 time_at_next_stop = self.time + waiting_time + travel_time
                 
                 nodes.add(Node(self, next_stop, time_at_next_stop, line))
         
         return nodes
-        
-    def h_cost(self, destination_stop):
-        distance = self.stop.distance(destination_stop)
-        return int(distance / MAXIMUM_SPEED)
-        
-    def g_cost(self):
-        return self.time
-        
-    def f_cost(self, destination_stop):
-        return self.g_cost() + self.h_cost(destination_stop)
-        
+            
     def __eq__(self, other):
         return self.stop == other.stop and self.time == other.time
     
@@ -61,23 +51,11 @@ class Stop(object):
         total_distance = sqrt(x_distance*x_distance + y_distance*y_distance)
         
         return total_distance
-        
-    def __eq__(self, other):
-        return self.number == other.number
-    
-    def __hash__(self):
-        return hash(self.number)
 
 class Transportation(object):
     def __init__(self, name):
         self.name       = name
         self.time_table = []
-    
-    def __eq__(self, other):
-        return self.name == other.name
-        
-    def __hash__(self):
-        return hash(self.name)
             
     def get_time_spent_on_arrival_at(self, stop):
         for entry in self.time_table:
@@ -159,34 +137,35 @@ class Transportation(object):
                 return self.time_table[i][0]
         
         return None
+
+
+from Queue import PriorityQueue
+
 def astar(from_stop, to_stop):
-    from Queue import PriorityQueue
+    def fcost(node, target):
+        g_cost = node.time
+        h_cost = int(node.stop.distance(target) / MAXIMUM_SPEED)
+        
+        return g_cost + h_cost
     
     start_node  = Node(None, from_stop, 0)
-    closed_list = set()
+
+    node_list = PriorityQueue()
+    node_list.put( (fcost(start_node, to_stop), start_node) )
     
-    open_list   = PriorityQueue()
-    open_list.put( (start_node.f_cost(to_stop), start_node) )
-    
-    found = False
     target_node = None
     
-    while not open_list.empty():
-        f_cost, current = open_list.get()
-        closed_list.add(current)
+    while not node_list.empty():
+        f_cost, current = node_list.get()
         
         if current.stop == to_stop:
-            found = True
             target_node = current
             break
         
         for adjacent_node in current.adjacent_nodes():
-            if adjacent_node in closed_list:
-                continue
-            
-            open_list.put( (adjacent_node.f_cost(to_stop), adjacent_node) )
+            node_list.put( (fcost(adjacent_node, to_stop), adjacent_node) )
         
-    if found:
+    if target_node:
         path = []
         current = target_node
         
@@ -198,55 +177,10 @@ def astar(from_stop, to_stop):
         return path
     else:
         return None
-
-def read_data():
-    stops_by_name   = dict()
-    stops_by_number = dict()
-    
-    f = open("data/pysakit.txt", "r")
-    for stop_data in f.readlines():
-        stop_data = stop_data.strip()
-        
-        if stop_data == "":
-            continue
-        
-        number, y_coord, x_coord, name = stop_data.split(" ")
-        number  = int(number)
-        y_coord = int(y_coord)
-        x_coord = int(x_coord)
-        stop    = Stop(number, y_coord, x_coord, name)
-        
-        stops_by_name[name]     = stop
-        stops_by_number[number] = stop
-        
-    f.close()
-    
-    lines = set()
-    
-    for line_name in ["11", "11b", "1a", "1ab", "3b", "3t", "55k", "55kb", "7a", "7b", "M", "Mb"]:
-        line = Transportation(line_name)
-        
-        f = open("data/linja%s.txt" % line_name, "r")
-        for time_table_entry in f.readlines():
-            time_table_entry = time_table_entry.strip()
-            
-            if time_table_entry == "":
-                continue
-            
-            stop_number, time_spent_str = time_table_entry.split(" ")
-            stop_number = int(stop_number)
-            time_spent  = int(time_spent_str)
-            stop        = stops_by_number[stop_number]
-            
-            line.add_time_table_entry(stop, time_spent)
-        
-        f.close()
-        lines.add(line)
-                
-    return (stops_by_name, stops_by_number, lines)
             
 if __name__ == "__main__":
     import sys
+    from io import read_data
     
     if len(sys.argv) < 3:
         print "Usage: python %s FROM_STOP TO_STOP" % sys.argv[0]
